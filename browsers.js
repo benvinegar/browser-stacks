9e939798a5e34dbc5465107a78ce1aa9f152f10f
Ben Vinegar <ben@benv.ca>
2015-11-23 16:42:40 -0800

    Use hardcoded list of browser capabilities

--- browsers.js
+++ browsers.js
@@ -1,52 +1,36 @@
-// returns available browsers to test on
-// results are keyed by browser
-// each browser has an array of objects with version and platform info
-// some versions may have more than one OS they can run on
-// {
-//     'chrome': [
-//        { version: 27, platform: 'Windows XP' }
-//        { version: 27, platform: 'Mac 10.6' }
-//     ]
-// }
+module.exports = [
+  { name: 'iphone', version: '5.1', platform: 'Mac 10.8' },
+  { name: 'iphone', version: '6.1', platform: 'Mac 10.8' },,
+  { name: 'iphone', version: '7.1', platform: 'Mac 10.10' },
+  { name: 'iphone', version: '8.4', platform: 'Mac 10.10' },
+  { name: 'iphone', version: '9.1', platform: 'Mac 10.10' },
 
-var https = require('https');
+  { name: 'firefox', version: '4', platform: 'Windows 2008' },
+  { name: 'firefox', version: '42', platform: 'Mac 10.10' },
 
-module.exports = function(cb) {
-    var info_opt = {
-        host: 'saucelabs.com',
-        path: '/rest/v1/info/browsers/webdriver'
-    };
+  { name: 'safari', version: '5', platform: 'Windows 2008' },
+  { name: 'safari', version: '6', platform: 'Mac 10.8' },
+  { name: 'safari', version: '7', platform: 'Mac 10.9' },
+  { name: 'safari', version: '8', platform: 'Mac 10.10' },
+  { name: 'safari', version: '9', platform: 'Mac 10.11' },
 
-    https.get(info_opt, function(res) {
-        res.setEncoding('utf8');
-        var body = '';
+  { name: 'chrome', version: '46', platform: 'Mac 10.10' },
 
-        res.on('data', function(data) {
-            body += data;
-        });
+  { name: 'internet explorer', version: '8', platform: 'Windows 2008' },
+  { name: 'internet explorer', version: '9', platform: 'Windows 2008' },
+  { name: 'internet explorer', version: '10', platform: 'Windows 2008' },
+  { name: 'internet explorer', version: '11', platform: 'Windows 2008' },
 
-        res.once('end', function() {
-            try {
-                cb(null, format(JSON.parse(body)));
-            } catch (err) {
-                return cb(err);
-            }
-        });
-    });
-};
+  { name: 'microsoftedge', version: '20', platform: 'Windows 10' },
 
-function format(obj) {
-    var browsers = {};
-    obj.forEach(function(info) {
-        var name = info.api_name;
+  { name: 'opera', version: '11', platform: 'Windows 2008' },
+  { name: 'opera', version: '12', platform: 'Windows 2008' },
 
-        var browser = browsers[name] = browsers[name] || [];
-        browser.push({
-            name: name,
-            version: info.short_version,
-            platform: info.os,
-        });
-    });
-
-    return browsers;
-}
+  { name: 'android', version: '4.0', platform: 'Linux' },
+  { name: 'android', version: '4.1', platform: 'Linux' },
+  { name: 'android', version: '4.2', platform: 'Linux' },
+  { name: 'android', version: '4.3', platform: 'Linux' },
+  { name: 'android', version: '4.4', platform: 'Linux' },
+  { name: 'android', version: '5.0', platform: 'Linux' },
+  { name: 'android', version: '5.1', platform: 'Linux' }
+];
